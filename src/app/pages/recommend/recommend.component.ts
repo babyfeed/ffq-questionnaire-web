95d2c0b05be92454efba018245af0017689896e9
Amun1804 <gjoseph@jeg.local>
2020-06-30 13:45:43 -0400

    Updated admin recommendations and added ability to edit

--- src/app/pages/recommend/recommend.component.ts
+++ src/app/pages/recommend/recommend.component.ts
@@ -1,89 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { ResultsService } from 'src/app/services/results/results';
-import { FFQResultsResponse } from 'src/app/models/ffqresultsresponse';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { RecommendModalComponent } from 'src/app/components/recommend-modal/recommend-modal.component';
-import { MatDialog } from '@angular/material';
-import { NutrientsRecommendationsService } from 'src/app/services/nutrients-recommendations/nutrients-recommendations.service';
-import { FFQNutrientsRecommendations } from 'src/app/models/ffqnutrients-recommendations';
-import { ErrorDialogPopupComponent } from 'src/app/components/error-dialog-popup/error-dialog-popup.component';
-import { Router } from '@angular/router';
-import { FoodRecommendModalComponent } from 'src/app/components/food-recommend-modal/food-recommend-modal.component';
-import { FoodRecommendationsService } from 'src/app/services/food-recommendation-service/food-recommendations.service';
+import { ResultsService } from "src/app/services/results/results";
+import { FFQResultsResponse } from "src/app/models/ffqresultsresponse";
+import {Observable} from 'rxjs';
+import { Description } from 'src/app/models/ffqfooddescription';
 import { FoodDescriptionService } from 'src/app/services/food-description/food-description.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-recommend',
+  selector: 'app-recommend-parental',
   templateUrl: './recommend.component.html',
   styleUrls: ['./recommend.component.css']
 })
+
 export class RecommendComponent implements OnInit {
-  results: FFQResultsResponse[] = [];
 
-  constructor(public resultsService: ResultsService,
-              public nutrientsRecommendationsService: NutrientsRecommendationsService,
-              public foodRecommendationsService: FoodRecommendationsService,
-              public foodDescriptionService: FoodDescriptionService,
-              private modalService: NgbModal,
-              private errorDialog: MatDialog,
-              private router: Router, ) { }
+  showBracketFirst = true;
+  showBracketSecond = false;
+  showBracketThird = false;
 
-  recommendedNutrients: FFQNutrientsRecommendations[] = [];
+  results: Description[] = [];
+  show:any = [];
+  constructor(public foodDescriptionService: FoodDescriptionService) {}
 
   ngOnInit() {
     this.getAllResults();
   }
 
-  private getNutrientsRecommendations(questionnaireId: string) {
-    this.nutrientsRecommendationsService.getNutrientsRecommendationsByQuestionnaireId(questionnaireId).subscribe(
-      data => {
-        this.onModalRequest(questionnaireId);
-      },
-      error => {
-        const dialogRef = this.errorDialog.open(ErrorDialogPopupComponent);
-        dialogRef.componentInstance.title = error.error.message;
-        dialogRef.componentInstance.router = this.router;
-      }
-    );
-  }
-
-  private getFoodRecommendations(questionnaireId: string) {
-    this.foodRecommendationsService.getFoodRecommendationsByQuestionnaireId(questionnaireId).subscribe(
-      data => {
-        this.onModalRequestFood(questionnaireId);
-      },
-      error => {
-        const dialogRef = this.errorDialog.open(ErrorDialogPopupComponent);
-        dialogRef.componentInstance.title = error.error.message;
-        dialogRef.componentInstance.router = this.router;
-      }
-    );
-  }
+  currentDesc = this.results
 
   private getAllResults() {
-    this.resultsService.getAllResults().subscribe(data => {
-      data.map(response => {
-        this.results.push(response);
-      });
+     const list: Observable<Description[]> = this.foodDescriptionService.getAllFoodItems();
+     list.subscribe(m => {
+       this.results = m;
+     });
+  }
+
+  public showFirst() {
+    this.showBracketFirst = true;
+    this.showBracketSecond = false;
+    this.showBracketThird = false;
+  }
+  public showSecond() {
+    this.showBracketFirst = false;
+    this.showBracketSecond = true;
+    this.showBracketThird = false;
+  }
+  public showThird() {
+    this.showBracketFirst = false;
+    this.showBracketSecond = false;
+    this.showBracketThird = true;
+  }
+  editPost(obj,i){
+  console.log(obj);
+    let index = this.results.findIndex(o => o._id == obj._id);
+    if(index != -1)
+    this.show[i] = true;
+  }
+  updateDescription(res,i){
+    this.show[i] = false;
+    const list: Observable<Description[]> = this.foodDescriptionService.updateFoodItemDescription(res._id,res)
+    list.subscribe(m => {
+      this.results = m;
     });
   }
-
-  //private getAllFoodItems() {
- //   this.foodDescriptionService.getAllFoodItems().subscribe(data => {
-  //    data.map(response => {
-  //      this.results.push(response);
- //     });
-  //  });
- // }
-
-  onModalRequest(id: string): void {
-    const modalRef = this.errorDialog.open(RecommendModalComponent);
-    modalRef.componentInstance.id = id;
-  }
-
-  onModalRequestFood(id: string): void {
-    const modalRef = this.errorDialog.open(FoodRecommendModalComponent);
-    modalRef.componentInstance.id = id;
-  }
-
 }
