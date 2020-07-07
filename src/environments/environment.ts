d37006a98a5a7d57959ec7574373f8dd6b5c319c
Miguel Herrera <miguel_herrera@ultimatesoftware.com>
2020-07-07 16:07:26 -0400

    Additional feedback display always on

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,23 +2,23 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  thisUrl: 'http://localhost:4200',
-  apiUrl: 'http://localhost:4000',
-  foodServiceUrl: 'http://localhost:9090',
-  questionnaireServiceUrl: 'http://localhost:9080',
-  userServiceUrl: 'http://localhost:9070'
-};
-
 // export const environment = {
-//   production: true,
-//   thisUrl: 'https://ffqquestionnairewestatic.z22.web.core.windows.net',
-//   apiUrl: 'https://ffq-auth.azurewebsites.net',
-//   foodServiceUrl: 'https://ffq-food-item-service.azurewebsites.net',
-//   questionnaireServiceUrl: 'https://ffq-questionnaire-service.azurewebsites.net',
-//   userServiceUrl: 'https://ffq-user-service.azurewebsites.net'
+//   production: false,
+//   thisUrl: 'http://localhost:4200',
+//   apiUrl: 'http://localhost:4000',
+//   foodServiceUrl: 'http://localhost:9090',
+//   questionnaireServiceUrl: 'http://localhost:9080',
+//   userServiceUrl: 'http://localhost:9070'
 // };
+
+export const environment = {
+  production: true,
+  thisUrl: 'https://ffqquestionnairewestatic.z22.web.core.windows.net',
+  apiUrl: 'https://ffq-auth.azurewebsites.net',
+  foodServiceUrl: 'https://ffq-food-item-service.azurewebsites.net',
+  questionnaireServiceUrl: 'https://ffq-questionnaire-service.azurewebsites.net',
+  userServiceUrl: 'https://ffq-user-service.azurewebsites.net'
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
