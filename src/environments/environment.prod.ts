cbb38958cd32426ff6c541b5030350ebd6aff532
Miguel Herrera <miguel_herrera@ultimatesoftware.com>
2020-07-14 16:42:09 -0400

    Fixed the food service url to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   thisUrl: 'https://babyfeedweb.z13.web.core.windows.net',
   apiUrl: 'https://ffq-authentication-service.azurewebsites.net',
-  foodServiceUrl: 'http://ffq-fooditemservice.azurewebsites.net',
+  foodServiceUrl: 'https://ffq-fooditemservice.azurewebsites.net',
   questionnaireServiceUrl: 'https://ffq-questionnaireservice.azurewebsites.net',
   userServiceUrl: 'https://ffq-userservice.azurewebsites.net'
 };
