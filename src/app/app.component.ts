ee7470d75c7d0a7f8b538fc2cb761ed1e801a91a
Christian Diaz <cdiaz259@fiu.edu>
2020-09-29 19:01:50 -0400

    <BF-F2020-21> Update icons in the parental portal, tracking tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry }  from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
@@ -7,6 +9,20 @@ import { FlashMessagesService } from 'angular2-flash-messages';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer){
+    this.matIconRegistry.addSvgIcon(
+      "arrow_red",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/arrow_red.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "arrow_yellow",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/arrow_yellow.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "check_green",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/check_green.svg")
+    );
+  }
   title = 'ffq-questionnaire-web';
 
 }
