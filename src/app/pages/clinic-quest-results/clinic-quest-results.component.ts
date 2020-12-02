241722ac9b5663d19b895db9225e10c611443222
Mauricio Naranjo <mnara016@fiu.edu>
2020-12-01 19:26:04 -0500

    clinician can download ffq results

--- src/app/pages/clinic-quest-results/clinic-quest-results.component.ts
+++ src/app/pages/clinic-quest-results/clinic-quest-results.component.ts
@@ -7,10 +7,10 @@
 
 */
 
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ResultsService } from "src/app/services/results/results";
-import { FFQResultsResponse } from "src/app/models/ffqresultsresponse";
+import { ResultsService } from 'src/app/services/results/results';
+import { FFQResultsResponse } from 'src/app/models/ffqresultsresponse';
 import { Observable } from 'rxjs';
 import { NutrientConstants } from 'src/app/models/NutrientConstants';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
@@ -32,19 +32,22 @@ import { Router } from '@angular/router';
 import { FoodRecommendModalComponent } from 'src/app/components/food-recommend-modal/food-recommend-modal.component';
 import { FoodRecommendationsService } from 'src/app/services/food-recommendation-service/food-recommendations.service';
 import { QuestionnaireValidatorService } from '../../services/questionnaire-validator/questionnaire-validator.service';
+import {ExportService} from '../../services/export/export-service';
+import {FFQFoodRecommendations} from '../../models/ffqfood-recommendations';
 
 @Component({
-  selector: "app-quest-results",
-  templateUrl: "./clinic-quest-results.component.html",
-  styleUrls: ["./clinic-quest-results.component.css"]
+  selector: 'app-quest-results',
+  templateUrl: './clinic-quest-results.component.html',
+  styleUrls: ['./clinic-quest-results.component.css']
 })
 export class ClinicQuestResultsComponent implements OnInit {
-  public show: boolean = false;
-  public showFeedback: boolean = false;
+  public show = false;
+  public showFeedback = false;
   feedbackForm: FormGroup;
   loading = false;
   submitted = false;
   search: string;
+  loggedInUser = this.authenticationService.currentUserValue;
 
 
   results: FFQResultsResponse[] = [];
@@ -58,6 +61,7 @@ export class ClinicQuestResultsComponent implements OnInit {
   resultInfo: FFQParentResult[] = [];
 //
 
+
   constructor(
     public resultsService: ResultsService,
     public clinicService: ClinicService,
@@ -69,7 +73,9 @@ export class ClinicQuestResultsComponent implements OnInit {
     public foodRecommendationsService: FoodRecommendationsService,
     private errorDialog: MatDialog,
     private formBuilder: FormBuilder,
-    private router: Router,) {}
+    private router: Router,
+    private exportService: ExportService
+    ) {}
 
   ngOnInit() {
     this.feedbackForm = this.formBuilder.group({
@@ -78,8 +84,10 @@ export class ClinicQuestResultsComponent implements OnInit {
 
     this.getClinicId();
   }
-
-  //loadData function serves to store the result and parent names into the FFQParentResult object
+  export() {
+    this.exportService.exportFFQResults(this.results, 'FFQ_Results');
+  }
+  // loadData function serves to store the result and parent names into the FFQParentResult object
   //                  serves to display the questionnaire-result data using the specification based on PO's list
   private loadData() {
 
@@ -102,16 +110,16 @@ export class ClinicQuestResultsComponent implements OnInit {
       newList.forEach(a =>  {
           newWeeklyMap.set(a, weeklyMap[a]);
           newDailyMap.set(a, dailyMap[a]);
-      })
+      });
 
       element.weeklyTotals = newWeeklyMap;
       element.dailyAverages = newDailyMap;
-      })
+      });
 
       this.results = m.reverse();
       this.parentNames = this.parentNames.reverse();
-      for(var i = 0; i < this.parentNames.length; i++){
-        var object: FFQParentResult = new FFQParentResult(
+      for (let i = 0; i < this.parentNames.length; i++){
+        let object: FFQParentResult = new FFQParentResult(
           this.results[i],
           this.parentNames[i]
         );
@@ -135,16 +143,16 @@ export class ClinicQuestResultsComponent implements OnInit {
       });
   }
 
-  //Function used to obtain the clinicId for the currently logged in clinician, in order to later display results based only for this specific clinic
+  // Function used to obtain the clinicId for the currently logged in clinician, in order to later display results based only for this specific clinic
   private getClinicId(){
 
-    var clinicListObervable: Observable<FFQClinicResponse[]> = this.clinicService.getAllClinics();
+    let clinicListObervable: Observable<FFQClinicResponse[]> = this.clinicService.getAllClinics();
     const loggedInUser = this.authenticationService.currentUserValue;
-    var clinicId: string;
+    let clinicId: string;
 
     clinicListObervable.subscribe(clinicList => {
-      var clinic = clinicList.find(a => a.clinicId == loggedInUser[0].assignedclinic);
-      if(clinic){
+      let clinic = clinicList.find(a => a.clinicId == loggedInUser[0].assignedclinic);
+      if (clinic){
         this.clinicId = clinic.clinicId;
         this.currentClinicName = clinic.clinicname;
       }
@@ -153,51 +161,60 @@ export class ClinicQuestResultsComponent implements OnInit {
 
   }
 
-  //Function used to filter the parent list to hold only the parents that are assigned to that specific clinic
+  // Function used to filter the parent list to hold only the parents that are assigned to that specific clinic
 private getParentList(){
-  var parentListObervable: Observable<FFQParentResponse[]> = this.parentService.getAllParents();
+  let parentListObervable: Observable<FFQParentResponse[]> = this.parentService.getAllParents();
 
   parentListObervable.subscribe(parentList => {
      parentList.forEach(parent => {
-       if(parent.assignedclinic == this.clinicId){
+       if (parent.assignedclinic === this.clinicId){
          this.parentList.push(parent);
        }
-     })
+     });
      this.getResultsList();
 
   });
 }
-
-
-  //Function to get all the results for each parent
+  // Function to get all the results for each parent
 private getResultsList(){
-   var allResultsObservable: Observable<FFQResultsResponse[]> = this.resultsService.getAllResults();
+   let allResultsObservable: Observable<FFQResultsResponse[]> = this.resultsService.getAllResults();
    allResultsObservable.subscribe((allResults: FFQResultsResponse[]) => {
       this.parentList.forEach(parent => {
           allResults.forEach(result => {
-              if(result.userId == parent.userId){
+              if (result.userId === parent.userId && parent.assignedclinician === this.loggedInUser[0].userId){
                 this.resultList.push(result);
-                var parentName = parent.firstname + " " + parent.lastname;
+                let parentName = parent.firstname + ' ' + parent.lastname;
                 this.parentNames.push(parentName);
               }
           });
       });
       this.loadData();
+      this.setFoodList();
    });
 
  }
-
+  private setFoodList() {
+    this.results.forEach(result => {
+      let recommendedFood: FFQFoodRecommendations[] = [];
+      this.foodRecommendationsService.getFoodRecommendationsByQuestionnaireId(result.questionnaireId).subscribe(
+        data => {
+          recommendedFood.push(data);
+        },
+      );
+      result.foodRecList = recommendedFood;
+    });
+  }
   private returnZero(){
     return 0;
   }
 
-  //function used in HTML in order to display and hide questionnaire results
+  // function used in HTML in order to display and hide questionnaire results
   toggle(index) {
     this.resultInfo[index].ffqresult.show = !this.resultInfo[index].ffqresult.show;
   }
 
   toggleFeedback(index) {
-    //ffqfeedback
+    // ffqfeedback
     this.resultInfo[index].ffqresult.showFeedback = !this.resultInfo[index].ffqresult.showFeedback;
   }
 
@@ -233,13 +250,13 @@ private getResultsList(){
   }
 
 
-    //functions used in HTML to display the nutrient recommendation after clicking on the button
+    // functions used in HTML to display the nutrient recommendation after clicking on the button
   onModalRequest(id: string): void {
     const modalRef = this.errorDialog.open(RecommendModalComponent);
     modalRef.componentInstance.id = id;
   }
 
-    //functions used in HTML to display the food recommendation after clicking on the button
+    // functions used in HTML to display the food recommendation after clicking on the button
   onModalRequestFood(id: string): void {
     const modalRef = this.errorDialog.open(FoodRecommendModalComponent);
     modalRef.componentInstance.id = id;
