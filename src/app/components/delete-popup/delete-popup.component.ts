fff174d913655144a45ef4534c145a48ad56b3eb
Mauricio Naranjo <mnara016@fiu.edu>
2021-02-17 01:26:20 -0500

    <BF-S2021-29> create second prompt when deleting something to ask if admin is sure

--- src/app/components/delete-popup/delete-popup.component.ts
+++ src/app/components/delete-popup/delete-popup.component.ts
@@ -42,6 +42,7 @@ export class DeletePopupComponent implements OnInit {
   isResearch = false;
   isResearch_institution = false;
   isQuestionnaire = false;
+  hidden = true;
 
   ngOnInit() {
     if (this.service == 'Clinician') { this.isClinician = true; }
@@ -123,17 +124,15 @@ export class DeletePopupComponent implements OnInit {
     }
     else if (this.isQuestionnaire){
       const questionnaireId = (this.attributes as FFQResultsResponse).questionnaireId;
-
       this.resultsService.
         deleteItem(questionnaireId)
-          .subscribe((data) => {
-            this.router.navigateByUrl('/admin/users');
+          .subscribe(() => {
             const dialogRef = this.errorDialog.open(ErrorDialogPopupComponent);
             dialogRef.componentInstance.title =
               'Result ' + questionnaireId + ' was deleted';
+            window.location.reload();
           });
     }
-
     this.activeModal.close('closed');
   }
 
