275efec8000702944bc2d77d91b75a77b6beb7c1
cdau001 <70779022+cdau001@users.noreply.github.com>
2021-02-23 11:37:59 -0500

    Update research-participant-service.ts

--- src/app/services/research-participant/research-participant-service.ts
+++ src/app/services/research-participant/research-participant-service.ts
@@ -55,7 +55,8 @@ export class ResearcherParticipantService {
             item.assignedResearcherUsers,
             item.childrennames,
             item.isactive,
-            item.userpassword
+            item.userpassword,
+			item.prefix
           );
         });
       })
