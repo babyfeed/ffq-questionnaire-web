b6d62c2beea909f0eeef9153f67fd79efbf6ff5f
Alberto Juan Canete <Acane017@fiu.edu>
2021-05-25 03:44:22 -0400

    Added more Translations towards the locale/message.es.xtf folder, Also added some new lines of code for translating more things, and also preparing for routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , LOCALE_ID, Inject } from '@angular/core';
 import { MatIconRegistry }  from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { FlashMessagesService } from 'angular2-flash-messages';
@@ -9,7 +9,7 @@ import { FlashMessagesService } from 'angular2-flash-messages';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer){
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, @Inject(LOCALE_ID) protected localeId: string) {
     this.matIconRegistry.addSvgIcon(
       "up_arrow",
       this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/up_arrow.svg")
@@ -24,5 +24,9 @@ export class AppComponent {
     );
   }
   title = 'ffq-questionnaire-web';
+  languageList = [
+    { code: 'en-US', label: 'English' },
+    { code: 'es', label: 'Español' }
+  ];
 
 }
