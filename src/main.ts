0cf077f3a29a071221433f1773bb92cd00d41598
Alberto Juan Canete <32527854+Shodam@users.noreply.github.com>
2021-05-26 08:05:02 -0400

    Revert "Alberto Canete - Wanting to try out new settings on Azure Production."

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { LOCALE_ID } from '@angular/core';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, { providers: [{ provide: LOCALE_ID, useValue: 'en-US' }] } )
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
