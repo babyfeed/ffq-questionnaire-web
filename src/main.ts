41ac194b2f606294eb538363251d77f10d118195
Alberto Juan Canete <Acane017@fiu.edu>
2021-05-27 17:59:47 -0400

    Changed Localization to false.

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic([{ provide: LOCALE_ID, useValue: 'en-US' }]).bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+//platformBrowserDynamic([{ provide: LOCALE_ID, useValue: 'en-US' }]).bootstrapModule(AppModule)
+  //.catch(err => console.error(err));
+
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
