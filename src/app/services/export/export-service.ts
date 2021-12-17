212daf27fb6c36917f22c6a6c24cccc95eb26af9
Wenjia1215 <65235107+Wenjia1215@users.noreply.github.com>
2021-12-17 11:40:14 -0500

    complete read timesOfReading from the response list

--- src/app/services/export/export-service.ts
+++ src/app/services/export/export-service.ts
@@ -1,14 +1,13 @@
-
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 
-import { FFQResultsResponse } from 'src/app/models/ffqresultsresponse';
-import { FoodRecommendationsService } from 'src/app/services/food-recommendation-service/food-recommendations.service';
-import { FFQFoodRecommendations } from 'src/app/models/ffqfood-recommendations';
-import { createHostListener } from '@angular/compiler/src/core';
+import {FFQResultsResponse} from 'src/app/models/ffqresultsresponse';
+import {FoodRecommendationsService} from 'src/app/services/food-recommendation-service/food-recommendations.service';
+import {FFQFoodRecommendations} from 'src/app/models/ffqfood-recommendations';
+import {createHostListener} from '@angular/compiler/src/core';
 import {FFQParent} from "../../models/ffqparent";
-import { TrackerResultsResponse } from 'src/app/models/trackerresultsresponse';
+import {TrackerResultsResponse} from 'src/app/models/trackerresultsresponse';
 
 @Injectable({
   providedIn: 'root'
@@ -18,19 +17,20 @@ export class ExportService {
 
   constructor(
     public foodRecommendationsService: FoodRecommendationsService
-  ) { }
+  ) {
+  }
 
   fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
   fileExtension = '.xlsx';
 
-  public exportTrackingHistory(results: TrackerResultsResponse[], parentList: string[], fileName: string): void{
+  public exportTrackingHistory(results: TrackerResultsResponse[], parentList: string[], fileName: string): void {
     const trackingHistory: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.getTrackingJSON(results, parentList));
-    const wb: XLSX.WorkBook = { Sheets: { TrackingHistory : trackingHistory }, SheetNames: ['TrackingHistory'] };
-    const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const wb: XLSX.WorkBook = {Sheets: {TrackingHistory: trackingHistory}, SheetNames: ['TrackingHistory']};
+    const excelBuffer: any = XLSX.write(wb, {bookType: 'xlsx', type: 'array'});
     this.saveExcelFile(excelBuffer, fileName);
   }
 
-  private getTrackingJSON(results: TrackerResultsResponse[], parentList: string[]): any{
+  private getTrackingJSON(results: TrackerResultsResponse[], parentList: string[]): any {
 
     var resultRows = [];
 
@@ -42,10 +42,10 @@ export class ExportService {
         'Age(Months)': result.age
       };
 
-      for(let item of result.responses){
+      for (let item of result.responses) {
         resultCol[item['food']] = item['response'];
       }
-      
+
       resultRows.push(resultCol)
 
     });
@@ -54,32 +54,32 @@ export class ExportService {
   }
 
   // had to make a new function because the input lists for both lists are in different formats; can't index with userID in clinic
-  public exportClinicTrackingHistory(results: TrackerResultsResponse[], parentList: string[], fileName: string): void{
+  public exportClinicTrackingHistory(results: TrackerResultsResponse[], parentList: string[], fileName: string): void {
     const trackingHistory: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.getClinicTrackingJSON(results, parentList));
-    const wb: XLSX.WorkBook = { Sheets: { TrackingHistory : trackingHistory }, SheetNames: ['TrackingHistory'] };
-    const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const wb: XLSX.WorkBook = {Sheets: {TrackingHistory: trackingHistory}, SheetNames: ['TrackingHistory']};
+    const excelBuffer: any = XLSX.write(wb, {bookType: 'xlsx', type: 'array'});
     this.saveExcelFile(excelBuffer, fileName);
   }
 
-  private getClinicTrackingJSON(results: TrackerResultsResponse[], parentList: string[]): any{
+  private getClinicTrackingJSON(results: TrackerResultsResponse[], parentList: string[]): any {
 
-    var resultRows = [];
-    var parentIndex = 0;
+    let resultRows = [];
+    let parentIndex = 0;
 
     results.forEach(result => {
 
-      var resultCol = {
+      const resultCol = {
         'Participant Username': parentList[parentIndex++],
-        'Date': result.date,
+        Date: result.date,
         'Age(Months)': result.age,
-        'Goal': result.goal
+        Goal: result.goal
       };
 
-      for(let item of result.responses){
-        resultCol[item['food']] = item['response'];
+      for (const item of result.responses) {
+        resultCol[item.food] = item.response;
       }
-      
-      resultRows.push(resultCol)
+
+      resultRows.push(resultCol);
 
 
     });
@@ -92,13 +92,53 @@ export class ExportService {
     const nutrients: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.getNutrientJson(results, parentList));
     const foodGroups: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.getFoodGroupsJson(results, parentList));
     const foods: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.getFoodsJson(results, parentList));
-    const wb: XLSX.WorkBook = { Sheets: { Nutrients: nutrients, FoodGroups: foodGroups, Foods: foods }, SheetNames: ['Nutrients', 'FoodGroups', 'Foods'] };
-    const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const wb: XLSX.WorkBook = {
+      Sheets: {Nutrients: nutrients, FoodGroups: foodGroups, Foods: foods},
+      SheetNames: ['Nutrients', 'FoodGroups', 'Foods']
+    };
+    const excelBuffer: any = XLSX.write(wb, {bookType: 'xlsx', type: 'array'});
     this.saveExcelFile(excelBuffer, fileName);
   }
 
   // Creates json object with nutrient sheet rows and collumns of data
   private getNutrientJson(results: FFQResultsResponse[], parentList: FFQParent[]): any {
+
+    // Array of rows of data
+    const resultRows = [];
+    const parentListData = parentList;
+    results.forEach(result => {
+      // Initialize columns with general result information
+      const resultCol = {
+        'Participant Username': parentList.find(parent => parent.userId === result.userId)?.username ?? '[not found]',
+        'Questionnaire ID': result.questionnaireId,
+        Date: result.date,
+        Name: result.patientName,
+        Age: result.ageInMonths,
+        ReadRecommend: parentList.find(parent => parent.userId === result.userId)?.lastReadRecommend,
+        TimesOfReading: parentList.find(parent => parent.userId === result.userId)?.timesOfReading,
+      };
+
+      // Add columns with nurient data
+      for (const key of result.dailyAverages.keys()) {
+        // Protection against undefined daily averages
+        resultCol[key] = result.dailyAverages.get(key);
+        if (typeof resultCol[key] === 'number') {
+          resultCol[key] = resultCol[key].toFixed(2);
+        } else {
+          resultCol[key] = 0.0;
+        }
+      }
+
+      // Push columns to array of rows
+      resultRows.push(resultCol);
+
+    });
+
+    return resultRows;
+
+  }
+
+  private getFoodsJson(results: FFQResultsResponse[], parentList: FFQParent[]): any {
 
     // Array of rows of data
     var resultRows = [];
@@ -109,64 +149,27 @@ export class ExportService {
       var resultCol = {
         'Participant Username': parentList.find(parent => parent.userId === result.userId)?.username ?? "[not found]",
         'Questionnaire ID': result.questionnaireId,
-        'Date': result.date,
-        'Name': result.patientName,
-        'Age': result.ageInMonths,
-        'ReadRecommend': parentList.find(parent => parent.userId === result.userId) ?.lastReadRecommend };
+        Date: result.date,
+        Name: result.patientName,
+        Age: result.ageInMonths
+      };
 
       // Add columns with nurient data
-      for (let key of result.dailyAverages.keys()) {
-        // Protection against undefined daily averages
-        resultCol[key] = result.dailyAverages.get(key);
-        if (typeof resultCol[key] == 'number') {
-          resultCol[key] = resultCol[key].toFixed(2);
-        }
-        else {
-           resultCol[key] = 0.0
-        }             
-      }
+      result.userChoices.forEach(choice => {
+
+        resultCol[choice.name + ' frequency'] = choice.frequency;
+        resultCol[choice.name + ' frequency type'] = choice.frequencyType;
+        resultCol[choice.name + ' servings'] = choice.serving;
+
+      });
+
 
       // Push columns to array of rows
-      resultRows.push(resultCol)
+      resultRows.push(resultCol);
 
     });
 
     return resultRows;
-
-  }
-
-  private getFoodsJson(results: FFQResultsResponse[], parentList: FFQParent[]): any {
-
-        // Array of rows of data
-        var resultRows = [];
-
-        results.forEach(result => {
-
-          // Initialize columns with general result information
-          var resultCol = {
-            'Participant Username': parentList.find(parent => parent.userId === result.userId)?.username ?? "[not found]",
-            'Questionnaire ID': result.questionnaireId,
-            'Date': result.date,
-            'Name': result.patientName,
-            'Age': result.ageInMonths
-          };
-
-          // Add columns with nurient data
-          result.userChoices.forEach(choice => {
-
-            resultCol[choice.name + " frequency"] = choice.frequency
-            resultCol[choice.name + " frequency type"] = choice.frequencyType
-            resultCol[choice.name + " servings"] = choice.serving
-
-          });
-
-
-          // Push columns to array of rows
-          resultRows.push(resultCol)
-
-        });
-
-        return resultRows;
 
   }
 
@@ -181,13 +184,13 @@ export class ExportService {
       var resultCol = {
         'Participant Username': parentList.find(parent => parent.userId === result.userId)?.username ?? "[not found]",
         'Questionnaire ID': result.questionnaireId,
-        'Date': result.date,
-        'Name': result.patientName,
-        'Age': result.ageInMonths
+        Date: result.date,
+        Name: result.patientName,
+        Age: result.ageInMonths
       };
 
       // Add columns with nurient data
-      result.foodRecList.forEach( res => {
+      result.foodRecList.forEach(res => {
         res.foodCategoryRecList.forEach(food => {
           resultCol[food.categoryName] = food.calculatedAmount.toFixed(2);
         });
@@ -205,8 +208,8 @@ export class ExportService {
   public exportExcel(jsonData: any[], fileName: string): void {
 
     const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData);
-    const wb: XLSX.WorkBook = { Sheets: { 'data': ws }, SheetNames: ['data'] };
-    const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const wb: XLSX.WorkBook = {Sheets: {data: ws}, SheetNames: ['data']};
+    const excelBuffer: any = XLSX.write(wb, {bookType: 'xlsx', type: 'array'});
     this.saveExcelFile(excelBuffer, fileName);
   }
 
