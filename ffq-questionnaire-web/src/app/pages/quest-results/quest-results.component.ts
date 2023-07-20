8a1411e7a6ff3b9bd36701d2c50d194e5db48ec1
Raxxar1201 <mugar015@fiu.edu>
2023-07-20 15:58:48 -0700

    loader maybe working?

--- ffq-questionnaire-web/src/app/pages/quest-results/quest-results.component.ts
+++ ffq-questionnaire-web/src/app/pages/quest-results/quest-results.component.ts
@@ -11,7 +11,7 @@ import {RecommendModalComponent} from 'src/app/components/recommend-modal/recomm
 import {MatDialog} from '@angular/material/dialog';
 import {NutrientsRecommendationsService} from 'src/app/services/nutrients-recommendations/nutrients-recommendations.service';
 import {ErrorDialogPopupComponent} from 'src/app/components/error-dialog-popup/error-dialog-popup.component';
-import {Router} from '@angular/router';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import {FoodRecommendModalComponent} from 'src/app/components/food-recommend-modal/food-recommend-modal.component';
 import {FoodRecommendationsService} from 'src/app/services/food-recommendation-service/food-recommendations.service';
 import {FoodDescriptionService} from 'src/app/services/food-description/food-description.service';
@@ -34,7 +34,18 @@ import { FFQDQIS } from 'src/app/models/ffqdqis';
 import { DQISModalComponent } from 'src/app/components/dqis-modal/dqis-modal.component';
 
 
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
 
+  loader.classList.add("loader--hidden");
+
+  loader.addEventListener("transitionend", () => {
+    const parentElement = loader.parentElement;
+    if (parentElement) {
+      parentElement.removeChild(loader);
+    }
+  });
+});
 // Questionnaire reesults page added by Daykel Muro 09/30/2019
 @Component({
   selector: 'app-quest-results',
@@ -99,6 +110,22 @@ export class QuestResultsComponent implements OnInit {
     });
 
     this.getAllResults();
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        // Show the loader when navigation starts
+        this.showLoader();
+      } else if (event instanceof NavigationEnd) {
+        // Hide the loader when navigation ends
+        this.hideLoader();
+      }
+    });
+  }
+  showLoader() {
+    this.show = true; // Or any other property you use to control the visibility of the loader in the template
+  }
+  
+  hideLoader() {
+    this.show = false; // Or set the property to false to hide the loader
   }
 
   // (Khalid)Changed below code to sort the list in the nutient view page
@@ -321,12 +348,3 @@ export class QuestResultsComponent implements OnInit {
   }
 }
 
-window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader");
-
-  loader.classList.add("loader--hidden");
-
-  loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader);
-  });
-});
